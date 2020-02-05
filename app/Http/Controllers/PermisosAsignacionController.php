d3eeec1c0e0b872d25689fe072abdb0246b4330a
Juan Manuel Acuña <gps1mx@gmail.com>
2020-02-04 19:48:15 -0600

    Agregado paquete bouncer

--- app/Http/Controllers/PermisosAsignacionController.php
+++ app/Http/Controllers/PermisosAsignacionController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Permissions;
-use App\PermissionsAssign;
 use Flash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Input;
